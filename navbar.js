aacb5a9e9c8332f119368838b5c4a6574e588a53
Nhan Cao <adidaphat.nhan@gmail.com>
2025-02-19 18:31:58 -0500

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -4,7 +4,7 @@ window.onscroll = function() {
 };
 
 function scrollFunction() {
-  if (document.body.scrollTOp > 80 || document.documentElement.scrollTop > 80) {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("navbar").style.padding = "30px 10px";
     document.getElementById("logo").style.padding = "25px";
   }
