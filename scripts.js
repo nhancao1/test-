17f1177472fb1998d69b27610346ecb675e4e39e
Nhan Cao <adidaphat.nhan@gmail.com>
2025-02-19 18:48:15 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,10 @@ function toggleSkills(skillType) {
     var skills = document.querySelector('.' + skillType);
     var toggleBtn = skills.previousElementSibling;
 
+    // Toggle the hidden class to show/hide the skills section
     skills.classList.toggle('hidden');
+
+    // Update the button icon based on the visibility of the skills section
     if (skills.classList.contains('hidden')) {
         toggleBtn.innerHTML = '<i class="fas fa-angle-down"></i>';
     } else {
